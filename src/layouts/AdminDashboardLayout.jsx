0a44622b0f6946a0060ced51f3f313778253f527
Umar Jimoh <umarjimoh@hotmail.com>
2025-05-19 16:50:24 +0100

    Add static  content to dashboard

--- src/layouts/AdminDashboardLayout.jsx
+++ src/layouts/AdminDashboardLayout.jsx
@@ -7,18 +7,18 @@ function AdminDashboardLayout({ children }) {
   const { isMobileSidebarOpen } = use(SidebarContext);
 
   return (
-    <div className="flex min-h-screen static">
+    <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] min-h-screen">
       <div
         className={`${
           isMobileSidebarOpen
-            ? "absolute z-40 top-12 right-0 bg-white px-4 py-12"
+            ? "absolute z-40 top-12 bottom-0 right-0 bg-white px-4 py-12"
             : "fixed opacity-0"
-        } md:flex md:opacity-100 z-40 md:-translate-x-full bg-white md:relative md:px-0 md:py-0 md:top-0 md:translate-none shadow-lg`}>
+        } md:flex md:opacity-100 z-40 bg-white md:relative md:px-0 md:py-0 md:top-0 md:translate-none`}>
         <SideBar />
       </div>
-      <div className="flex flex-col flex-1">
+      <div>
         <Navbar />
-        <div className="bg-[#dee9da] flex-1">{children}</div>
+        <div className="p-8 lg:p-10 h-full bg-[#e6f3df] text-[#343a40]">{children}</div>
       </div>
     </div>
   );
